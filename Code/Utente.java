80df9ae1eded5a7865fbc5dee51f74cf36b00f2f
Annagloria01 <annagloria05@gmail.com>
2026-04-28 15:19:53 +0200

    classe utente

---
+++ Code/Utente.java
@@ -0,0 +1,33 @@
+package Code;
+
+import java.util.ArrayList;
+
+public class Utente {
+    private String nome;
+    private String idUtente;
+    protected ArrayList<Risorsa> risorseInPrestito;
+
+    public Utente(String nome, String idUtente) {
+        this.nome = nome;
+        this.idUtente = idUtente;
+        this.risorseInPrestito = new ArrayList<>();
+    }
+
+    // Metodo per prendere in prestito
+    public void prendiInPrestito(Risorsa r) {
+        risorseInPrestito.add(r);
+    }
+
+    // Metodo per restituire
+    public void restituisciRisorsa(Risorsa r) {
+        risorseInPrestito.remove(r);
+    }
+
+    // Metodo per stampare la lista
+    public void stampaRisorse() {
+        System.out.println("Risorse di " + nome + ":");
+        for (Risorsa r : risorseInPrestito) {
+            System.out.println("- " + r.getTitolo()); // Assumendo che Risorsa abbia getTitolo()
+        }
+    }
+}
