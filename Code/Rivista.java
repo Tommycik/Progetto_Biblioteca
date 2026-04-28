dac824619fc11c81ecb95a21771954c5362115ee
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-28 15:18:06 +0200

    risorsa

---
+++ Code/Rivista.java
@@ -0,0 +1,26 @@
+package Code;
+
+public class Rivista extends Risorsa{
+    private int numero;
+    //costruttore
+    public Rivista(String titolo, int annoPubblicazione, String codice, int numero) {
+        super(titolo, annoPubblicazione, codice);
+        this.numero = numero;
+    }
+    //getter e setter
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    //override stampa risorsa
+    @Override
+    public void visualizzaDettagli() {
+        System.out.print("Titolo: " + this.getTitolo() + " Anno: " + this.getAnnoPubblicazione() + " Codice: " + this.getCodice() + " Numero: " + this.getNumero());
+    }
+
+    
+}
